4d190d50a8999182e89629bc8f8676735a6e1bb0
alfinsuryaS <benjaminstwo@gmail.com>
2020-04-27 18:15:32 +0800

    Build to production

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // to 'development' as per webpack documentation. Again, you don't have to
   // use webpack or any other bundler during development! Recheck README if
   // you didn't know this
-  mode: 'development',
+  mode: 'production',
   output: {
     path: path.join(__dirname, "dist"),
     filename: 'index.production.js',
